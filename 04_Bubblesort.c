d3e96801f8dd035563137b8e85985b5e40ba1a7a
ceccs17d39 <43641954+ceccs17d39@users.noreply.github.com>
2018-11-27 21:17:18 +0530

    Update 04_Bubblesort.c

--- 04_Bubblesort.c
+++ 04_Bubblesort.c
@@ -1,12 +1,18 @@
 /*Nithya Venugopal
   S3 D/* 39
   Bubblesort*/
+
+
 #include <stdio.h>
-int bubbleSort(int array[], int size) {
+int bubbleSort(int array[], int size) 
+{
 	int i, j, temp;
-	for (i=size; i>=0; i--) {
-		for (j=0; j<i; j++) {
-			if (array[j] > array[j+1]) {
+	for (i=size; i>=0; i--) 
+	{
+		for (j=0; j<i; j++) 
+		{
+			if (array[j] > array[j+1]) 
+			{
 				temp = array[j];
 				array[j] = array[j+1];
 				array[j+1] = temp;
@@ -14,21 +20,21 @@ int bubbleSort(int array[], int size) {
 		}
 	}
 }
-int main() {
+int main() 
+{
 	int i, j, array[100], size, temp;
-
 	printf("Enter number of elements in the array : ");
 	scanf("%d", &size);
-
 	printf("Enter the array\n");
-	for (i=0; i<size; i++) {
+	for (i=0; i<size; i++) 
+	{
 		scanf("%d", &array[i]);
 	}
 
 	bubbleSort(array, size);
-
 	printf("Sorted array \n");
-	for (i=0; i<size; i++) {
+	for (i=0; i<size; i++) 
+	{
 		printf("%d\n", array[i]);
 	}
 }
