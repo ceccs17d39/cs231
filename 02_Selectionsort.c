6d0c285f295a943bec78b461d74c6ff529ad1c5b
ceccs17d39 <43641954+ceccs17d39@users.noreply.github.com>
2018-11-27 21:12:31 +0530

    Update and rename selectionsort.c to 02_Selectionsort.c

--- 02_Selectionsort.c
+++ 02_Selectionsort.c
@@ -1,16 +1,24 @@
+/*Nithya Venugopal
+  S3 D 39
+  Selection Sort*/
+
+
 #include <stdio.h>
-
-int selectionSort(int array[], int size) {
+int selectionSort(int array[], int size) 
+{
 	int i, j, pos, temp;
-	for (i=0; i<size; i++) {
+	for (i=0; i<size; i++) 
+	{
 		pos = i;
-
-		for (j=i; j<size; j++) {
-			if (array[j] < array[pos]) {
+		for (j=i; j<size; j++) 
+		{
+			if (array[j] < array[pos]) 
+			{
 				pos =j;
 			}
 		}
-		if (i != pos) {
+		if (i != pos) 
+		{
 			temp = array[i];
 			array[i] = array[pos];
 			array[pos] = temp;
@@ -18,21 +26,21 @@ int selectionSort(int array[], int size) {
 	}
 }
 
-int main() {
+int main() 
+{
 	int i, j, pos, array[100], temp, size;
-
 	printf("Enter number of elements in the array : ");
 	scanf("%d", &size);
-
 	printf("Enter the array\n");
-	for (i=0; i<size; i++) {
+	for (i=0; i<size; i++) 
+	{
 		scanf("%d", &array[i]);
 	}
 	
 	selectionSort(array, size);
-
 	printf("Sorted array \n");
-	for (i=0; i<size; i++) {
+	for (i=0; i<size; i++)
+	{
 		printf("%d\n", array[i]);
 	}
 }
