e0feceef306e1ebe9acf85e0be64619b3d1d9258
ceccs17d39 <43641954+ceccs17d39@users.noreply.github.com>
2018-11-30 11:39:11 +0530

    Update 01_SequentialSearch.c

--- 01_SequentialSearch.c
+++ 01_SequentialSearch.c
@@ -7,14 +7,14 @@
 void main()
 {
 	int ar[50],k,i,n,p=0;
-	printf("Enter the size");
+	printf("Enter the size of array:");
 	scanf("%d",&n);
-	printf("Enter elements\n");
+	printf("Enter elements:\n");
 	for(i=0;i<n;i++)
 	{
 		scanf("%d",&ar[i]);
 	}
-	printf("Enter number to be searched\n");
+	printf("Enter number to be searched:\n");
 	scanf("%d",&k);
 	for(i=0;i<n;i++)
 	{
@@ -29,3 +29,15 @@ void main()
 	else
 		printf("%d is not in the list\n",k);
 }
+
+Output
+Enter the size of array:5
+Enter elements:
+23
+32
+12
+9
+75
+Enter number to be searched:
+12
+12 is found in the list, at position 3
